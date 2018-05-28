224dd5ee5688bc8e4851f22b9a74bad8f1036790
caseycallow <casey@udacity.com>
2018-05-28 06:25:34 +0000

    add install script to automate vim setup/symlinks

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# go to home directory
+
+cd ~/
+
+echo configuring dotfiles...
+
+# symlink config files
+
+ln -sf .dotfiles/.bash_profile ~/.bash_profile
+echo .bash_profile symlinked
+
+ln -sf .dotfiles/.tmux.conf ~/.tmux.conf
+echo .tmux.conf symlinked
+
+ln -sf .dotfiles/.vimrc ~/.vimrc
+echo .vimrc symlinked
+
+# set up Vundle
+
+mkdir ~/.vim
+cd .vim
+mkdir bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+echo vim packages installed
+
+echo Success! All dotfiles symlinked and configured.
