7f85f5f29e3f027c73c73f18f6e280e2fae97282
caseycallow <casey@udacity.com>
2018-06-13 04:11:13 +0000

    cleanup echos

--- install.sh
+++ install.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
 # go to home directory
-
 cd ~
 
-echo configuring dotfiles...
-
 # symlink config files
-
 ln -sf .dotfiles/.bash_profile ~/.bash_profile
-echo .bash_profile symlinked
-
 ln -sf .dotfiles/.tmux.conf ~/.tmux.conf
-echo .tmux.conf symlinked
-
 ln -sf .dotfiles/.vimrc ~/.vimrc
-echo .vimrc symlinked
 
 # set up Vundle
-
 mkdir ~/.vim
 cd .vim
 mkdir bundle
@@ -26,11 +16,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # set up fzf
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-source ~/.bashrc ~./bash_profile
 
-echo vim packages installed
-
-echo Success! All dotfiles symlinked and configured.
+echo Environment configured successfully ✔
